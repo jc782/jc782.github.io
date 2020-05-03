f3b506fe4b5d0e681ef281d5099a6a563fe62261
Josh Carr <josh.carr@cantab.net>
2020-05-03 09:52:58 +0100

    add georgia and centre svg

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,6 @@ function onSVGLoaded( data ){
     var path = data.select("#flight");
     var s = Snap("#someID");
     s.append( data );
-    var bbox = s.getBBox();
-    var viewBox = bbox.x+' '+bbox.y+' '+bbox.width+' '+ bbox.height;
     path.animate({
       "stroke-dashoffset": -300,
     }, 4000, mina.easeinout);
