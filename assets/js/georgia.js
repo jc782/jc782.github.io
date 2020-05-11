259226d219814529dabb711b6e0c6c180e9debd1
Josh Carr <josh.carr@cantab.net>
2020-05-11 22:40:00 +0100

    add georgia animation

--- assets/js/georgia.js
+++ assets/js/georgia.js
@@ -1,12 +1,37 @@
-window.onload= function(){
-var world = Snap.load("https://joshcarr.co.uk/assets/svg/Georgia.svg", onSVGLoaded ) ;
+window.onload = function () {
+  var world = Snap.load("https://joshcarr.co.uk/assets/svg/test.svg", onSVGLoaded);
+  var s = Snap("#someID");
+  let last_known_scroll_position = 0;
+  let ticking = false;
+  var limit = document.getElementById("scroll").scrollHeight
+  console.log(limit)
+  window.addEventListener('scroll', function (e) {
+    last_known_scroll_position = window.scrollY;
+    this.console.log(window.scrollY)
+    if (!ticking) {
+      window.requestAnimationFrame(function () {
+        doSomething(window.scrollY, limit)
+        ticking = false;
+      });
+
+      ticking = true;
+    }
+  }
+  );
 };
 
-function onSVGLoaded( data ){ 
-    var path = data.select("#flight");
-    var s = Snap("#someID");
-    s.append( data );
-    path.animate({
-      "stroke-dashoffset": -300,
-    }, 4000, mina.easeinout);
+function onSVGLoaded(data) {
+  var path = data.select("#flight");
+  var s = Snap("#someID");
+  s.append(data);
+}
+
+function doSomething(scroll_pos, limit) {
+  var s = Snap("#someID");
+  var path = s.select("#linearGradient6854")
+  var elev = s.select("#path2549")
+  var dimens = elev.getBBox();
+  var len = dimens.width;
+  var value = len * (scroll_pos / parseFloat(limit));
+  path.attr({ "x2": value, "x1": 0, "y2": 200, "y1": 200, });
 }
