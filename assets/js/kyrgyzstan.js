6302e45c057bbea6c4b9b17d7e7aa4d6a3e07c56
Josh Carr <josh.carr@cantab.net>
2020-05-13 22:29:41 +0100

    add kyrgyzstan animation

--- assets/js/kyrgyzstan.js
+++ assets/js/kyrgyzstan.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-  var world = Snap.load("http://localhost:4000/assets/svg/kyr.svg", onSVGLoaded);
+  var world = Snap.load("https://joshcarr.co.uk/assets/svg/kyr.svg", onSVGLoaded);
   var s = Snap("#someID");
   let last_known_scroll_position = 0;
   let ticking = false;
@@ -28,8 +28,8 @@ function onSVGLoaded(data) {
 
 function doSomething(scroll_pos, limit) {
   var s = Snap("#someID");
-  var path = s.select("#linearGradient6854")
-  var elev = s.select("#path2549")
+  var path = s.select("#linearGradient5274")
+  var elev = s.select("#path5249")
   var dimens = elev.getBBox();
   var len = dimens.width;
   var value = len * (scroll_pos / parseFloat(limit));
